19845fb35f57e2a0cad450a260c4ce190d15172b
nancyorgan <nancy.organ@vanderbilt.edu>
2014-06-23 10:14:32 -0500

    Add nancys_app

--- server.R
+++ server.R
@@ -2,9 +2,9 @@ shinyServer(function(input, output) {
    
   output$final_score = renderText({
      input$submit
-     paste(isolate(as.numeric(input$q1) + as.numeric(input$q2) + 
+     paste(isolate(round(as.numeric(input$q1) + as.numeric(input$q2) + 
       as.numeric(input$q3) + as.numeric(input$q4) +
-      as.numeric(input$q5)) , "/5")
+      as.numeric(input$q5),1)) , "/5")
       })
   
   output$logo = renderUI({
